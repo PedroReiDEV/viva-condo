a95f42eea36e121b4a50532e3cea1e9a8982e253
Pedro <pedrosfs098@gmail.com>
2025-08-15 21:41:16 -0300

    função

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@
 
 export default function Home() {
 
-  const element = <span>{2+2}</span>;
+type Morador = {
+  primeiroNome: string;
+  sobrenome: string;
+}
+
+  function formatarNomeMorador(morador:Morador) {
+    return morador.primeiroNome + ' ' + morador.sobrenome
+  }
+  const morador = {
+    primeiroNome: 'Sandro',
+    sobrenome: 'pereira'
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">{element}</h1>
+    <h1 className="text-4x1 font-bold">{formatarNomeMorador(morador)}</h1>
     </div>
-  );
+  )
 }
