c04be4e6d47b2f1f3f7ccb17acc551ace2dc7d75
Pedro <pedrosfs098@gmail.com>
2025-08-22 21:26:32 -0300

    criado a api para trazer os dados do condominio

---
+++ src/services/api-condominios.ts
@@ -0,0 +1,18 @@
+export interface ICondominio {
+    id_condominio: string;
+    nome_condominio: string;
+    endereco_condominio: string;
+    cidade_condominio: string;
+    uf_condominio: string;
+    tipo_condominio: string;
+    created_at: string;
+    id_client: number;
+}
+
+export const getCondominio =async () =>{
+
+    const response = await fetch('https://raw.githubusercontent.com/vagner107/viva-condo/refs/heads/main/src/app/condominios/api_condominio.json')
+    //https://fatec.short.gy/condominios
+
+    return await response.json() //Converte a resposta em JSON
+}
